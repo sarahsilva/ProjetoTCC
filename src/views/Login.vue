8f674e9d83b2673577cb9bea07466baec156fb8a
Sarah <sarahjbsilva@gmail.com.com>
2019-04-21 20:01:47 -0300

    Continuação Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <v-text-field label="Teste"></v-text-field>
-        <v-btn color="red">Teste</v-btn>
-    </div>
+    <v-container grid-list-md text-xs-center>
+        <v-layout>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-text-field label="Código do usuário:"></v-text-field>
+                   
+                    <v-btn color="success">Entrar</v-btn>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
